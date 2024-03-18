117f273ed53b562fce9c8e345aac9a81c48d9568
Thomas Sobieck <6256765+Sobieck@users.noreply.github.com>
2024-03-18 10:37:45 -0500

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,17 +1,7 @@
-if (Get-Module -ListAvailable -Name posh-git) {
-    Write-Host "posh-git already installed"
-} 
-else {
-    Install-Module posh-git -Scope CurrentUser
-}
+winget install JanDeDobbeleer.OhMyPosh -s winget
 
-if (Get-Module -ListAvailable -Name oh-my-posh) {
-    Write-Host "oh-my-posh already installed"
-} 
-else {
-    Install-Module oh-my-posh -Scope CurrentUser
-}
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/stelbent-compact.minimal.omp.json" | Invoke-Expression
 
-Set-Theme DarkBlood
+oh-my-posh init pwsh | Invoke-Expression
 
 $env:Path += ";C:\tools"
