8feed4f4980a5a64735d9c8e6291b2b9a4fa1c5c
Thomas Sobieck <thomas@sobieck.com>
2025-02-03 12:24:33 -0600

    Add linux making of zip files

--- Microsoft.PowerShell_profile.ps1
+++
@@ -1,7 +0,0 @@
-winget install JanDeDobbeleer.OhMyPosh -s winget
-
-oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/stelbent-compact.minimal.omp.json" | Invoke-Expression
-
-oh-my-posh init pwsh | Invoke-Expression
-
-$env:Path += ";C:\tools"
